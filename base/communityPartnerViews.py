94a64ce3694eb8fbb01f17660ac59af42407a14b
Theo Tsapakos <ttsapakos@gmail.com>
2016-11-30 18:03:11 -0500

    Latest changes

--- base/communityPartnerViews.py
+++ base/communityPartnerViews.py
@@ -22,6 +22,7 @@ class CommunityPartnerDetail(APIView):
     def post(self, request, format=None):
         serializer = CommunityPartnerSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
