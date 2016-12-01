91ea7566e5fb5243f8d6ea71cb42112f30551346
sameerbarde <barde.s@husky.neu.edu>
2016-11-30 19:21:06 -0500

    Database Schema Completion and Test Data Creation

--- DB Schema files/TestData_Insert.sql
+++ DB Schema files/TestData_Insert.sql
@@ -1,36 +1,34 @@
 -- Administrator --
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Lisa', 'Service', 'service-learning@northeastern.edu', 'servicelearning4life', 'admin', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('servicelearning4life', '2016-11-20', 1, 'lisa@servicelearning.com', 'Lisa', 'Service', '', 0, 1, '2016-08-12');
 
 -- Students --
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Sameer', 'Barde', 'barde.s@husky.neu.edu', 'password123', 'student', true, '2016-11-20', '2016-09-01' );
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-20', 0, 'barde.s@husky.neu.edu', 'Sameer', 'Barde', '', 0, 1, '2016-09-01' );
 
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Joe', 'Chen', 'chen.jo@husky.neu.edu', 'password123', 'student', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-20', 0, 'chen.jo@husky.neu.edu', 'Joe', 'Chen', '', 0, 1, '2016-09-01');
 
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Luke', 'Starr', 'starr.lu@husky.neu.edu', 'password123','student', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-20', 0, 'starr.l@husky.neu.edu', 'Luke', 'Starr', '', 0, 1, '2016-09-01');
     
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Ryan', 'Lough', 'lough.r@husky.neu.edu', 'password123', 'student', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-24', 0, 'lough.r@husky.neu.edu', 'Ryan', 'Lough', '', 0, 1, '2016-09-01');
 
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Theodore', 'Tsapakos', 'tsapakos.t@husky.neu.edu', 'password123', 'student', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-20', 0, 'tsapakos.t@husky.neu.edu', 'Theodore', 'Tsapakos', '', 0, 1, '2016-09-01');
 
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Erik', 'Kaasila', 'kaasila.e@husky.neu.edu', 'password123', 'student', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-10', 0, 'kaasila.e@husky.neu.edu', 'Erik', 'Kaasila', '', 0, 1, '2016-09-01');
     
 -- Instructor --
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Mike', 'Weintraub', 'weintraub.m@northeastern.edu', 'password123', 'instructor', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-10', 0, 'weintraub.m@northeastern.edu', 'Michael', 'Weintraub', '', 1, 1, '2016-09-01');
 
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Kevin', 'James', 'james.k@northeastern.edu', 'password123', 'student, instructor', true, '2016-11-20', '2016-09-01');
+insert into auth_user (password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+	values ('password123', '2016-11-10', 0, 'james.k@northeastern.edu', 'Kevin', 'James', '', 1, 1, '2016-09-01');
     
-insert into User (first_name, last_name, username, pwd, groups, is_active, last_login, date_joined)
-	values ('Kevin', 'Smith', 'james.s@northeastern.edu', 'password123', 'instructor', false, '2015-11-20', '2012-09-01');
-    
+
 -- College --
 insert into College (name)
 	values ('College of Computer and Information Science');
@@ -49,39 +47,39 @@ insert into College (name)
 
 -- Department --
 insert into Department (name, college_id)
-	values ('Computer Science' , 1001);
+	values ('Computer Science' , 'College of Computer and Information Science');
     
 insert into Department (name, college_id)
-	values ('Biology' , 1003);
+	values ('Biology' , 'College of Science');
 
 insert into Department (name, college_id)
-	values ('Mathematics' , 1003);
+	values ('Mathematics' , 'College of Science');
     
 
 -- Course --
-insert into Course (course_number, name, department_id)
-	values ('CS4500', 'Software Development', 1001);
+insert into Course (id, name, department_id)
+	values ('CS4500', 'Software Development', 'Computer Science');
 
-insert into Course (course_number, name, department_id)
-	values ('CS1200', 'Overview 1', 1001);
+insert into Course (id, name, department_id)
+	values ('CS1200', 'Overview 1', 'Computer Science');
     
 -- Semester --
 insert into Semester (name, start_date, end_date, is_active)
 	values ('FALL2016', '2016-08-31', '2016-12-25', true);
     
 insert into Semester (name, start_date, end_date, is_active)
-	values ('SPRG2017', '2017-01-08', '2017-06-01', false);
+	values ('SPRING2017', '2017-01-08', '2017-06-01', false);
 
 
 -- Enrollment --
-insert into Enrollment (user_id, course_number, semester_name, meeting_days, meeting_start_time, meeting_end_time, is_active)
-	values (102, 'CS4500', 'FALL2016', 'MWR', '08:00', '09:15', true);
+insert into Enrollment (user_id, course_id, semester_id, crn, is_active)
+	values (2, 'CS4500', 'FALL2016', '12235', true);
 	
-insert into Enrollment (user_id, course_number, semester_name, meeting_days, meeting_start_time, meeting_end_time, is_active)
-	values (103, 'CS1200', 'SPRG2017', 'T', '02:50', '04:30', false);
+insert into Enrollment (user_id, course_id, semester_id, crn, is_active)
+	values (3, 'CS1200', 'SPRING2017', '44221', false);
 	
-insert into Enrollment (user_id, course_number, semester_name, meeting_days, meeting_start_time, meeting_end_time, is_active)
-	values (108, 'CS4500', 'FALL2016', 'TF', '09:50' ,'11:30', true);
+insert into Enrollment (user_id, course_id, semester_id, crn, is_active)
+	values (8, 'CS4500', 'FALL2016', '56323', true);
 	
 -- Community Partner --
 insert into Community_Partner (name)
@@ -91,11 +89,11 @@ insert into Community_Partner (name)
 	values ('America Scores');
 	
 -- Project --
-insert into Project (name, course_number, community_partner_id, start_date, end_date, description, location)
-	values ('TimeTracker', 'CS4500', 1001, '2016-09-01', '2016-12-20', 'Time Tracker device for Service Learning', Point(42.3398, 71.0892));
+insert into Project (name, course_id, community_partner_id, start_date, end_date, description, longitude, latitude)
+	values ('TimeTracker', 'CS4500', 1001, '2016-09-01', '2016-12-20', 'Time Tracker device for Service Learning', 42.3398, 71.0892);
 	
-insert into Project (name, course_number, community_partner_id, start_date, end_date, description, location)
-	values ('America Scores', 'CS4500', 1002, '2016-01-01', '2017-01-01', null, null);
+insert into Project (name, course_id, community_partner_id, start_date, end_date, description, longitude, latitude)
+	values ('America Scores', 'CS4500', 1002, '2016-01-01', '2017-01-01', null, null, null);
 	
 -- Record_Category --
 insert into Record_Category (name)
@@ -111,32 +109,26 @@ insert into Record_Category (name)
 	values ('Team Planning/Group Work');
 	
 -- Record --
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (1 , 1001, '2016-08-25', null, 3, null, 2, true, 'Worked at Project A', null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (1 , 1001, '2016-08-25', null, 3, null, null, 2, true, 'Worked at Project A', null);
 	
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (2, 1001, '2016-08-25', '08:00', 8.3, null, 2, true, 'Worked at Project A', null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (2, 1001, '2016-08-25', '08:00', 8.3, null, null, 2, true, 'Worked at Project A', null);
 	
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (1, 1001, '2016-08-25', '08:00', 10, Point(42.3399, 71.0891), 2, false, 'Worked at Project A', null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (1, 1001, '2016-08-25', '08:00', 10, 42.3399, 71.0891, 2, false, 'Worked at Project A', null);
 	
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (1, 1001, '2016-08-28', '08:00', 1.20, Point(42.3399, 71.0891), 2, true, 'Worked at Project A', null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (1, 1001, '2016-08-28', '08:00', 1.20, 42.3399, 71.0891, 2, true, 'Worked at Project A', null);
 	
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (2, 1002, '2016-08-25', '08:00', 5.4, null, 2, false, 'Worked at Project B', null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (2, 1002, '2016-08-25', '08:00', 5.4, null, null, 2, false, 'Worked at Project B', null);
 	
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (1, 1002, '2016-10-25', '11:00', 5, null, 2, true, null, null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (1, 1002, '2016-10-25', '11:00', 5, null, null, 2, true, null, null);
 	
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (1, 1002, '2016-09-28', '08:00', 4, Point(42.3399, 71.0881), 2, false, 'Worked at Project B', null);
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (1, 1002, '2016-09-28', '08:00', 4, 42.3399, 71.0881, 2, false, 'Worked at Project B', null);
 
-insert into Record (enrollment_id, project_id, date, start_time, total_hours, location, category_id, is_active, comments, extra_field)
-	values (1, 1002, '2016-09-28', '08:00', 10, Point(42.3299, 71.0884), 2, true, 'Worked at Project B', null);
-
-	
-	
-	
-	
-	
+insert into Record (enrollment_id, project_id, date, start_time, total_hours, longitude, latitude, category_id, is_active, comments, extra_field)
+	values (1, 1002, '2016-09-28', '08:00', 10, 42.3299, 71.0884, 2, true, 'Worked at Project B', null);
