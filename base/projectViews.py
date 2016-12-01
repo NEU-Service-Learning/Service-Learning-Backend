7d94a866d8cb85a2bfe6309d06daa6aee4e1efbf
Theo Tsapakos <ttsapakos@gmail.com>
2016-11-30 20:17:28 -0500

    Added projects

--- base/projectViews.py
+++ base/projectViews.py
@@ -6,50 +6,39 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 
-from base.communityPartnerSerializer import CommunityPartnerSerializer
 from base.projectSerializer import ProjectSerializer
 
-class CommunityPartnerDetail(APIView):
+class ProjectDetail(APIView):
     """
-    Post, retrieve, update or delete a community partner instance
+    Post, retrieve, update or delete a project instance
     """
     def get_object(self, pk):
         try:
-            return CommunityPartner.objects.get(pk=pk)
-        except CommunityPartner.DoesNotExist:
+            return Project.objects.get(pk=pk)
+        except Project.objects.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request, format=None):
-        serializer = CommunityPartnerSerializer(data=request.data)
+        serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, pk, format=None):
-        communityPartner = self.get_object(pk)
-        communityPartner = CommunityPartnerSerializer(communityPartner);
-        return Response(communityPartner.data);
+        project = self.get_object(pk)
+        project = ProjectSerializer(project);
+        return Response(project.data);
 
     def put(self, request, pk, format=None):
-        communityPartner = self.get_object(pk)
-        serializer = CommunityPartnerSerializer(communityPartner, data=request.data)
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        communityPartner = self.get_object(pk)
-        communityPartner.delete()
+        project = self.get_object(pk)
+        project.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-'''
-class CommunityPartnerProjects(APIView):
-    """
-    Retrieve all projects for a community partner
-    """
-    def get(self, request, pk, format=None):
-        serializer = ProjectSerializer(Project.objects.filter(community_partner=pk))
-        return Response(serializer.data)
-'''
--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from . import communityPartnerViews
+from . import projectViews
 
 urlpatterns = [
     #url(r'/$', views.Instructors.as_view(), name='instructors'),
@@ -9,7 +10,10 @@ urlpatterns = [
     #url(r'users/$', views.Users.as_view()),
     #url(r'users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'communityPartner/$', communityPartnerViews.CommunityPartnerDetail.as_view()),
-    url(r'communityPartner/(?P<pk>[0-9]+)/$', communityPartnerViews.CommunityPartnerDetail.as_view())
+    url(r'communityPartner/(?P<pk>[0-9]+)/$', communityPartnerViews.CommunityPartnerDetail.as_view()),
+    url(r'communityPartner/(?P<pk>[0-9]+)/projects/$', communityPartnerViews.CommunityPartnerProjects.as_view()),
+    url(r'project/$', projectViews.ProjectDetail.as_view()),
+    url(r'project/(?P<pk>[0-9]+)/$', projectViews.ProjectDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
