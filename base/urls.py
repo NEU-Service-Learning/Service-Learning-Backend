9bb4bb3560ae085712044b116f1543940c05deb5
Erik Kaasila <ekaasila@gmail.com>
2016-12-01 17:38:39 -0500

    wip #2

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
 from . import communityPartnerViews
 from . import projectViews
 from . import enrollment_views
--- base/user_views.py
+++ base/user_views.py
@@ -16,18 +16,6 @@ class UserList(APIView):
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        user = self.get_object(pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 class UserDetail(APIView):
     """
     Retrieve, update or delete a user instance.
@@ -45,7 +33,7 @@ class UserDetail(APIView):
 
     def put(self, request, pk, format=None):
         user = self.get_object(pk)
-        serializer = UserSerializer(user, data=request.DATA)
+        serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
