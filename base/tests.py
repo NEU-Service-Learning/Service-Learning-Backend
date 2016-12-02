5a86cdfd08a2adb2846599c179239f98677a17a1
Starr <luke.starr@harvardbusiness.org>
2016-12-01 21:04:11 -0500

    Tests plz

--- base/tests.py
+++ base/tests.py
@@ -13,15 +13,14 @@ class ExampleMethodTests(TestCase):
 	def test_basic_example_strings(self):
 		self.assertIs("this is a test", "this is a test")
 
-		from django.test import TestCase
-
+		
 class SemesterTests(TestCase):
 	def setUp(self):
 		self.client = Client()
 	
 	def basic_post_semester(self):
 	
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name": "FALL2016",
 			"start date": "2016-09-01",
@@ -31,7 +30,7 @@ class SemesterTests(TestCase):
 		self.assertEqual(semester.status_code, 201)
 		self.assertEqual(semester.context['name'], "FALL2016")
 		self.assertEqual(semester.context['is active'], true)
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name": "FALL2017",
 			"start date": "2017-09-01",
@@ -45,7 +44,7 @@ class SemesterTests(TestCase):
 	def bad_post_semester(self):
 	
 		# Semester with inconsistent dates
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name": "FALL2016",
 			"start date": "2016-12-31",
@@ -55,7 +54,7 @@ class SemesterTests(TestCase):
 		self.assertEqual(semester.status_code, 422)
 		
 		# Semester with non-string name
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name": True,
 			"start date": "2016-09-01",
@@ -65,7 +64,7 @@ class SemesterTests(TestCase):
 		self.assertEqual(semester.status_code, 422)
 		
 		# Semester with non-date start
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name":	 "FALL2016",
 			"start date": "Yesterday",
@@ -75,7 +74,7 @@ class SemesterTests(TestCase):
 		self.assertEqual(semester.status_code, 422)
 		
 		# Semester with non-date end
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name":	 "FALL2016",
 			"start date": "2016-09-01",
@@ -87,7 +86,7 @@ class SemesterTests(TestCase):
 	def basic_put_semester(self):
 	
 		# Set up semester
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name":	 "FALL2016",
 			"start date": "2016-09-01",
@@ -112,7 +111,7 @@ class SemesterTests(TestCase):
 	def bad_put_semester(self):
 		
 		# Set up semester
-		semester = self.client.add(/semester/,
+		semester = self.client.add('/semester/',
 		{
 			"name":	 "FALL2016",
 			"start date": "2016-09-01",
@@ -155,7 +154,7 @@ class SemesterTests(TestCase):
 	def start_semester(self):
 		
 		# Set up current semester
-		first_semester = self.client.add(/semester/,
+		first_semester = self.client.add('/semester/',
 		{
 			"name": "FALL2016",
 			"start date": "2016-09-01",
@@ -167,7 +166,7 @@ class SemesterTests(TestCase):
 		self.assertEqual(first_semester.context['is active'], true)
 		
 		# Set up the semester to transition to
-		second_semester = self.client.add(/semester/,
+		second_semester = self.client.add('/semester/',
 		{
 			"name": "FALL2017",
 			"start date": "2017-09-01",
