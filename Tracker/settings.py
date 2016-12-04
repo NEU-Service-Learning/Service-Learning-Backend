7585d902caa2772627be701c8a3048fc1e661cb8
Erik Kaasila <ekaasila@gmail.com>
2016-12-04 17:42:56 -0500

    remove CSRF check, this is really bad, undo this in future.

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'base.CsrfExemptSessionAuthentication.CsrfExemptSessionAuthentication',
     )
 }
 
