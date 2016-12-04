fcef42540b12e5e74456c103c8ad38eee8c2a81c
Starr <luke.starr@harvardbusiness.org>
2016-12-04 18:33:38 -0500

    tests

--- base/tests_semester.py
+++ base/tests_semester.py
@@ -150,7 +150,7 @@ class SemesterTests(TestCase):
 			"is_active": 'false'
 		})		
 		self.user = User(username="kename.f@neu.edu", first_name="Fa", password="password1")
-        self.user.save()
+		self.user.save()
 		enrollment = Enrollment(user=self.user, course=self.course, semester="FALL2016", meeting_days="MWF", meeting_start_time=datetime.now().time(), meeting_end_time=datetime.now().time(), crn="12345")
 		enrollment.save()
 		enrollment = Enrollment(user=self.user, course=self.course, semester="SPRING2017", meeting_days="MWF", meeting_start_time=datetime.now().time(), meeting_end_time=datetime.now().time(), crn="12345")
