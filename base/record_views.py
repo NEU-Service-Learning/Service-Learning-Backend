e9e26c85bf81da8b1ffdfef271534b15057da8d5
sameerbarde <barde.s@husky.neu.edu>
2016-12-04 21:45:09 -0500

    Added code coverage for Record

--- base/record_views.py
+++ base/record_views.py
@@ -122,3 +122,18 @@ class RecordHoursForCourse(APIView):
         else:
             records = Record.objects.filter(is_active=True, course=course, date=[start_date, end_date])\
                 .aggregate(Sum('total_hours'))
+
+
+class RecordsExport(APIView):
+    """
+    """
+
+    def get(self, request, format=None):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="All-Records.csv"'
+        writer = csv.writer(response)
+        for record in Record.objects.filter(is_Active=True):
+            temp = RecordSerializer(record, many=False)
+            writer.writerow(temp.data.values())
+
+        return response
